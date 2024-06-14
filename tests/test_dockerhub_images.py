61c2ba613531d8d0e36a8bb2d589df54d64727ca
Andrey Dubinchik <evil_ghost@bk.ru>
2024-06-14 10:52:34 +0300

    Revert "update dockerhub check (#10)" (#11)
    This reverts commit bdfbd1839d0705932f7e9fe143c7bbb156317590.

--- tests/test_dockerhub_images.py
+++ tests/test_dockerhub_images.py
@@ -20,9 +20,9 @@ def test_dockerhub_images_exist(
         deploy_file_info: tuple[Path, str],
         deploy_info_file_content: dict[str, str],
         dockerhub_username_key: str
-) -> None:
+        ) -> None:
     common_part_of_link_to_docker_hub = (
-        'https://mirror.gcr.io/v2/namespaces/{username}/repositories/{image}/'
+        'https://hub.docker.com/v2/namespaces/{username}/repositories/{image}/'
     )
     expected_docker_images = (
         'kittygram_backend', 'kittygram_frontend', 'kittygram_gateway'

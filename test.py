1390425464667ab6d9992f044a13aaae22cb9841
Ilya <ilkanov106@gmail.com>
2024-12-18 08:31:51 +0800

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+X='watermelon','apple','melon','wildberris'
+
+print(X)
+print(ALLOWED_HOSTS)

fa8486cd979061af60a7dbec6abb1ba03f5c2cfb
Ilya <ilkanov106@gmail.com>
2024-12-22 22:19:53 +0800

    add bash scripts

---
+++ backend/backend_static_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo docker compose -f docker-compose.production.yml exec backend python manage.py migrate;
+sudo docker compose -f docker-compose.production.yml exec backend python manage.py collectstatic;
+sudo docker compose -f docker-compose.production.yml exec backend cp -r /app/collected_static/. /static/static/;

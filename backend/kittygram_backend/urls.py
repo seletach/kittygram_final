da6e5bea47240cb667628b483b9a6c20f7fedeae
Ilya <ilkanov106@gmail.com>
2024-12-25 01:48:51 +0800

    isort

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,10 +1,10 @@
-from cats.views import AchievementViewSet, CatViewSet
-from rest_framework import routers
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
+from cats.views import AchievementViewSet, CatViewSet
+from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)

bf6e9fe94781d16ee665bc5fe5a143bcde5d7a09
Ilya <ilkanov106@gmail.com>
2024-12-26 00:11:40 +0800

    review

--- backend/backend_static_script.sh
+++ backend/backend_static_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi;
 python manage.py migrate;
 python manage.py collectstatic;
 cp -r /app/collected_static/. /static/static/;
+exec "$@";
